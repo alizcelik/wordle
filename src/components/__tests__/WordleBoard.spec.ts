a2e13b6dc79a7db96583fba6de46a225b7cf3908
alizcelik <ali.ozcelik_01@metu.edu.tr>
2024-02-28 21:24:52 +0300

    extract constant

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,18 +1,15 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import {VICTORY_MESSAGE} from "../settings";
 
 describe('WordleBoard', () => {
   it("a victory message is displayed when the user makes a guess that matches the word of the day", async () => {
-    //arrange
     const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "Testing"}})
 
-    //act
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue("Testing")
     await guessInput.trigger('keydown.enter')
 
-    //assert
-    expect(wrapper.text()).toContain("You win!")
-
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 })
